d2da17e89b0fd709b105f2258eff47946e6813f3
sokampdx <sokam@pdx.edu>
2017-11-21 21:51:46 -0800

    Working tic-tac-toe

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.currentTurn();
     this.setState({
       squares: squares,
